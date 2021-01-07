6c785bdb00379896c45b36de9f0c8efa0a6df1fe
Dorian <68146134+ThePuddingCODE@users.noreply.github.com>
2021-01-07 13:11:50 +0100

    Update indexconfigconfig.php

--- files/congih/indexconfigconfig.php
+++ files/congih/indexconfigconfig.php
@@ -1,1 +1,8 @@
+<?php
 
+$keywords = ""; #Keywords in here like bli, blub, bla
+$author = ""; #Author of the Site here
+$discription = ""; #discription here
+$stylepathmain = ""; #style of the main stylesheet here
+
+?>
